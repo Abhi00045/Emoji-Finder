6634206fd65172b93066ca630e3fc9d3eccea3d3
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-08-07 01:52:57 +0530

    ok now sleep

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 
 // import emojiList from "./emoji.js";
+let parent = document.getElementById("tableBody");
 
-function displayEmoji(){
+function displayEmoji(searchedValue=""){
+    
+    let filteredList = emojiList.filter(
+        function(e){
+            if(e.category.indexOf(searchedValue)!=-1){
+               return true;
+            }
+            if(e.description.indexOf(searchedValue)!=-1){
+                return true;
+            }
+        }
+    );
 
-    emojiList.forEach((e)=>{
-        let parent = document.getElementById("tableBody");
+    console.log(filteredList);
+
+    filteredList.forEach((e)=>{
 
         let new_row=document.createElement("tr");
         let new_emoji=document.createElement("td");
-        //let new_description=document.createElement("td");
+        let new_description=document.createElement("td");
 
         new_emoji.innerText=e.emoji;
-        // new_description.innerText=e.description;
+        new_description.innerText=e.description;
 
         new_row.appendChild(new_emoji);
-        // new_row.appendChild(new_description);
+        new_row.appendChild(new_description);
 
         parent.appendChild(new_row);
-        new_row.style.display="flex"
-        new_row.style.flexDirection="row"
-        parent.style.display="flex";
-        parent.style.flexDirection="row";
-        parent.style.flexWrap="wrap";
-        new_row.style.gap="20px";
-        new_emoji.style.fontSize="30px"
+        // new_row.style.display="flex"
+        // new_row.style.flexDirection="row"
+        // parent.style.display="flex";
+        // parent.style.flexDirection="row";
+        // parent.style.flexWrap="wrap";
+        // new_row.style.gap="200px";
+        // new_emoji.style.fontSize="30px"
 
     });
 };
+window.addEventListener("load",displayEmoji(""));
+document.getElementById("searchField").addEventListener("keyup",(event)=>{
 
-let searchField = document.getElementById("searchField");
+    let searchedValue = document.getElementById("searchField").value;
+    // console.log(searchField);
+    displayEmoji(searchedValue);
+})
 
-function searchEmoji(){
-
-    let search = document.getElementById("searchField").value;
-    if(search==""){
-        displayEmoji();
-        alert("Please Enter Something");
-    }
-}
-
-search.addEventListener("keydown",searchEmoji);
-window.addEventListener("load",displayEmoji);
