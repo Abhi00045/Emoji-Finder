b602cde470eb0a7a4526c9b6fd0a0c8e62585462
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-08-10 17:56:18 +0530

    done not do

--- script.js
+++ script.js
@@ -3,33 +3,31 @@
 let body = document.querySelector("body");
 let parent = document.getElementById("tableBody");
 
+// console.log(emojiList)
 function displayEmoji(searchedValue){
-    
-    let filteredList = emojiList.filter(
-        function(e){
-            if(e.category.indexOf(searchedValue)!=-1){
-               return true;
-            }
-            if(e.description.indexOf(searchedValue)!=-1){
-                return true;
-            }
-        }
-    );
+    let filteredList = emojiList.filter(item => {
+         if (item.description.indexOf(searchedValue) != -1) {
+            return true;
+        };
+    return filteredList;
+});
 
-    console.log(filteredList);
+
+//   function renderEmoji(){
+console.log(filteredList);
+
 
     filteredList.forEach((e)=>{
-
         let new_row=document.createElement("tr");
         let new_emoji=document.createElement("td");
         let new_description=document.createElement("td");
-
+        
         new_emoji.innerText=e.emoji;
         new_description.innerText=e.description;
-
+        
         new_row.appendChild(new_emoji);
         new_row.appendChild(new_description);
-
+        
         parent.appendChild(new_row);
         new_row.style.display="flex"
         new_row.style.flexDirection="row"
@@ -39,19 +37,19 @@ function displayEmoji(searchedValue){
         new_row.style.gap="200px";
         new_emoji.style.fontSize="30px"
         new_emoji.style.cursor="pointer";
-
+        
         new_emoji.addEventListener("click",function(){
             navigator.clipboard.writeText(e.emoji);
             alert(`copied ${e.emoji} to clipboard`);
         })
-
+        
     });
-};
-window.addEventListener("load",displayEmoji(""));
-document.getElementById("searchField").addEventListener("keydown",(event)=>{
+}   
+    
 
-    let searchedValue = document.getElementById("searchField").value;
-    // console.log(searchField);
-    displayEmoji(searchedValue);
-})
+
+window.addEventListener("load",displayEmoji(""));
+
+document.getElementById("searchField").addEventListener("keydown", (e) => displayEmoji(e.target.value));
+
 
